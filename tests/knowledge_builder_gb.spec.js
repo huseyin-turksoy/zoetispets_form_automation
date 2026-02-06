39c89ac7f47c7bf2b37fc75b461008f365fffd54
huseyin turksoy <huseyin.turksoy@nmqdigital.com>
2026-02-07 00:37:35 +0300

    knowledge builder update

---
+++ tests/knowledge_builder_gb.spec.js
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+
+const urls = [
+  'https://www.zoetispets.com/en-gb/dog-healthcare/arthritis/knowledge-builder/',
+  'https://www.zoetispets.com/en-au/dog-healthcare/arthritis/knowledge-builder/',  
+  'https://www.zoetispets.com/en-ca/dog-healthcare/arthritis/knowledge-builder/',
+  'https://www.zoetispets.com/es-es/perro-salud/artrosis/knowledge-builder/',
+  'https://www.zoetispets.com/de-de/hund-gesundheit/arthrose/knowledge-builder/',
+  'https://www.zoetispets.com/pl-pl/ochrona-zdrowia-psa/osteoarthritis/knowledge-builder/',
+  'https://www.zoetispets.com/it-it/salute-cane/dolore-articolare/knowledge-builder/'
+];
+//const url = 'https://zoetispets.com/en-gb/dog-healthcare/arthritis/knowledge-builder/';
+
+urls.forEach((url) => {
+
+  test('filling the knowledge builder form for ' + url, async ({ page }) => {
+
+  await page.goto(url);
+  //await page.waitForLoadState('networkidle');
+
+  for(let i=0; i<2; i++){
+
+    if (await page.locator('button[id="onetrust-accept-btn-handler"]').isVisible()) {
+  await page.locator('button[id="onetrust-accept-btn-handler"]').first().click();
+  break;
+  }else if (await page.locator('.onetrust-close-btn-handler').first().isVisible()) {
+    await page.locator('.onetrust-close-btn-handler').first().click();    
+  }else{
+    await page.waitForTimeout(3000);
+  }
+
+  }
+
+  const ipPopupClose = page.locator('[class="ip-tracking-popup__close"]');
+
+  if (await ipPopupClose.isVisible()) {
+  await ipPopupClose.click();
+  } else{
+    console.log('IP popup not visible');
+  }
+  
+  const iframe = page.frameLocator('.ceros-experience');
+  await iframe.locator('button[id="6891cc8112900"]').click();
+  await iframe.locator('button[id="686b6de41ef08"]').click();
+  await iframe.locator('button[id="68768176b74d0"]').click();  
+  await iframe.locator('button[id="687646ecb7479"]').click();
+  //await iframe.locator('div[id="687646a4b2604"]').waitFor();
+  expect(await iframe.locator('div[id="687646a4b2604"]')).toBeTruthy();
+  await console.log('Knowledge Builder test completed successfully');
+  //await console.log(iframe.locator('div[id="687646a4b2604"]').allTextContents());
+
+  });
+});
